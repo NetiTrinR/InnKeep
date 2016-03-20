9138caf4d8a8cf6902905f745de192f7dde7f430
Michael Manning <michaelmanning@gmail.com>
2016-03-20 13:13:28 -0700

    Added tab index to login fields

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                             <label class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}" tabindex="1">
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -27,7 +27,7 @@
                             <label class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
-                                <input type="password" class="form-control" name="password">
+                                <input type="password" class="form-control" name="password" tabindex="2">
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
