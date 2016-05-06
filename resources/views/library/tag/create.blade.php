ead1c00283ecc098949373faa0e1c7d1c0622ed7
Michael Manning <michaelmanning390@gmail.com>
2016-05-06 14:48:57 -0600

    Created Tag create page

---
+++ resources/views/library/tag/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts._app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">New Tag</h3>
+                </div>
+                <div class="panel-body">
+                    <!-- Name Form Input -->
+                    <div class="form-group">
+                        <label for="name" class="control-label">Tag Name</label>
+                        <input type="text" name="name" class="form-control" />
+                    </div>
+                    <input type="submit" value="Create" class="btn btn-success pull-right">
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
